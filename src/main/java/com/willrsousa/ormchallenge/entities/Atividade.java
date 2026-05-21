3253914f3cef7dcd40475600d4d0f8f7e5b9cf54
William Sousa <williamrsousa94@hotmail.com>
2026-05-21 18:31:35 -0300

    Classe Bloco criada

--- src/main/java/com/willrsousa/ormchallenge/entities/Atividade.java
+++ src/main/java/com/willrsousa/ormchallenge/entities/Atividade.java
@@ -2,6 +2,9 @@ package com.willrsousa.ormchallenge.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_atividade")
 public class Atividade {
@@ -16,6 +19,9 @@ public class Atividade {
     @ManyToOne
     @JoinColumn(name="categoria_id")
     private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
 
     public Atividade(){}
 
@@ -65,5 +71,9 @@ public class Atividade {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
     }
 }
