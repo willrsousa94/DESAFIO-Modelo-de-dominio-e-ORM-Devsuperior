d1ac43c8ab0e915dedc4c0036723ab683585b5e1
William Sousa <williamrsousa94@hotmail.com>
2026-05-21 13:45:23 -0300

    criada primeira classe + banco h2 preparado

---
+++ src/main/java/com/willrsousa/ormchallenge/entities/Categoria.java
@@ -0,0 +1,36 @@
+package com.willrsousa.ormchallenge.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+
+    public Categoria(){}
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
