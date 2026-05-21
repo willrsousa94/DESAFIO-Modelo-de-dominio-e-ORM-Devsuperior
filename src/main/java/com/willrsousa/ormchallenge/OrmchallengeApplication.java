a522663dac0feb6f8c6ef30a3c1b3c8d1ec5449c
William Sousa <williamrsousa94@hotmail.com>
2026-05-21 12:01:32 -0300

    project initialized

---
+++ src/main/java/com/willrsousa/ormchallenge/OrmchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.willrsousa.ormchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willrsousa/ormchallenge/OrmchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willrsousa.ormchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
